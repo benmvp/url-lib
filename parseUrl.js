0916102c786c26d671700ad0de465e3f910301f1
Ben Ilegbodu <ben@benmvp.com>
2016-04-01 21:31:16 -0700

    Fix UMD scripts

--- parseUrl.js
+++ parseUrl.js
@@ -6,7 +6,7 @@
 */
 (function(root, factory) {
     if (typeof define === 'function' && define.amd) {
-        define([], factory);
+        define(factory);
     }
     else if (typeof module === 'object' && module.exports) {
         module.exports = factory();
